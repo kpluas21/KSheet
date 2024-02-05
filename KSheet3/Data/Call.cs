c547d06c69b05213b930a7565c18f43ebe57aaa8
Kevin Pluas <kpluas21@gmail.com>
2024-02-05 13:17:31 -0500

    Add project files.

---
+++ KSheet3/Data/Call.cs
@@ -0,0 +1,13 @@
+﻿namespace KSheet3.Data
+{
+	public class Call
+	{
+		public int Id { get; set; }
+		public int Position { get; set; }
+		public string? Address { get; set; }
+		public string? PdSignal { get; set; }
+		public string? FdSignal { get; set; }
+		public string? Notes { get; set; }
+
+	}
+}
---
+++ KSheet3/Data/CallContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KSheet3.Data
+{
+	public class CallContext : DbContext
+	{
+		protected readonly IConfiguration Configuration;
+
+		public CallContext(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite(Configuration.GetConnectionString("CallsDB"));
+		}
+
+		public DbSet<Call> Calls { get; set; }
+	}
+}
