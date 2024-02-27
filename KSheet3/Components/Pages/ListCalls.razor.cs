4bb304be2450e29968cd3e3c9fa403da24a385a2
vboxuser <vboxuser@KEVIN>
2024-02-27 18:38:13 -0500

    added SearchCallModel

--- KSheet3/Components/Pages/ListCalls.razor.cs
+++ KSheet3/Components/Pages/ListCalls.razor.cs
@@ -1,43 +1,76 @@
 ﻿using KSheet3.Data;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace KSheet3.Components.Pages
 {
+	public class CallSearchModel
+	{
+		public int? Id { get; set; }
+		public int? Position { get; set; }
+		public int? Incident { get; set; }
+		public string? Address { get; set; }
+		public string? PdSignal { get; set; }
+		public string? FdSignal { get; set; }
+		public string? Notes { get; set; }
+		public DateTime? TimeFrom { get; set; }
+		public DateTime? TimeTo { get; set; }
+
+	}
+
+
 	public partial class ListCalls
 	{
 		public CallContext? _context;
+
 		public List<Call>? searchResults { get; set; }
-		public Call? callQuery {  get; set; }
+		public CallSearchModel? callSearch {  get; set; }
 
 		protected override void OnInitialized()
 		{
-			callQuery = new Call();
+			callSearch = new CallSearchModel();
 		}
 
 		//SEARCH 
 
-		public async Task<List<Call>> Search()
+		public async Task Search()
 		{
 			_context ??= await CallContextFactory.CreateDbContextAsync();
 
 			if(_context != null)
 			{
-				IQueryable<Call> query = _context.Calls;
-				
-				if(!string.IsNullOrEmpty(callQuery?.Address))
+				searchResults = _context.Calls.ToList(
+
+				if(callSearch.Id.HasValue)
 				{
-					query = query.Where(m => m.Address.Contains(callQuery.Address));
+					result = result.Where(m => m.Id.Equals(callSearch.Id));
 				}
 
-				if(callQuery?.Position != 0) 
+				if(callSearch.Incident.HasValue)
 				{
-					query = query.Where(m => m.Position == callQuery.Position);
+					result = result.Where(m => m.Incident.Equals(callSearch.Incident));
 				}
-				searchResults =  await query.ToListAsync();
+
+				if(!string.IsNullOrEmpty(callSearch.Address))
+				{
+					result = result.Where(m => m.Address.Contains(callSearch.Address));
+				}
+
+				if(!string.IsNullOrEmpty(callSearch.PdSignal))
+				{
+					result = result.Where(m => m.PdSignal.Contains(callSearch.PdSignal));
+				}
+
+				if(!string.IsNullOrEmpty(callSearch.FdSignal))
+				{
+					result = result.Where(m => m.FdSignal.Contains(callSearch.FdSignal));
+				}
+
+				if(callSearch.Position.HasValue)
+				{
+					result = result.Where(m => m.Position.Equals(callSearch.Position));
+				}
 			}
-
-			return null;
-
 			
 		}
 
